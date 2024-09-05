f89eb84826d6a08f3c995a54b9395002b433e466
Yurii Ivanovskyi <freemanlutsk@gmail.com>
2024-09-05 11:03:42 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_serverlessapplicationrepository_cloudformation_stack" "this" {
   application_id   = local.application_id
-  capabilities     = ["CAPABILITY_NAMED_IAM","CAPABILITY_RESOURCE_POLICY"]
+  capabilities     = ["CAPABILITY_AUTO_EXPAND"]
   name             = var.name
   semantic_version = var.semantic_version
   tags             = var.tags
